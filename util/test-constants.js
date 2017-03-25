62730385558934df8b5a63783f5dd3d545a1001b
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-03-26 00:45:12 +0700

    lint

--- util/test-constants.js
+++ util/test-constants.js
@@ -2,13 +2,13 @@ export const SPEED = {
 	LOW: 20,
 	MEDIUM: 60,
 	HIGH: 100,
-  RANDOM: Math.floor(Math.random() * 100) + 1
+	RANDOM: Math.floor(Math.random() * 100) + 1
 };
 
 export const POINT = {
 	LATITUDE: -6.2191701,
 	LONGITUDE: 106.8129082,
-  RANDOM: Math.random() * 100
+	RANDOM: Math.random() * 100
 };
 
 export const RASH = {
