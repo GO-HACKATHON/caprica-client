09ac433838965c437d7916cdfad464ff93d8581e
Muhammad Mustadi <muhammad.mustadi@gmail.com>
2017-03-26 03:01:47 +0700

    lint

--- index.android.js
+++ index.android.js
@@ -4,7 +4,7 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {
   AppRegistry,
   StyleSheet,
@@ -12,42 +12,42 @@ import {
   View
 } from 'react-native';
 
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: '#F5FCFF'
+	},
+	welcome: {
+		fontSize: 20,
+		textAlign: 'center',
+		margin: 10
+	},
+	instructions: {
+		textAlign: 'center',
+		color: '#333333',
+		marginBottom: 5
+	}
+});
+
 export default class capricaClient extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
-        <Text style={styles.instructions}>
+				<Text style={styles.instructions}>
           To get started, edit index.android.js
         </Text>
-        <Text style={styles.instructions}>
+				<Text style={styles.instructions}>
           Double tap R on your keyboard to reload,{'\n'}
           Shake or press menu button for dev menu
         </Text>
-      </View>
-    );
-  }
+			</View>
+		);
+	}
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
 
 AppRegistry.registerComponent('capricaClient', () => capricaClient);
--- util/gojekapi.test.js
+++ util/gojekapi.test.js
@@ -7,7 +7,6 @@ it('can get nearby gojeks', async () => {
 	expect(nearbyGojeks.length).toBeGreaterThan(0);
 });
 
-
 it('can get nearby goCars', async () => {
 	const location = '-6.2635056,106.8236571';
 	const nearbyGojeks = await gApi.getNearbyGoCar({location});
